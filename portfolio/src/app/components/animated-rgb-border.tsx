deeb8ab6cf0f01169d60068be218636dd1044371
Joseph Monakedi <jimonakedi@gmail.com>
2025-02-27 01:55:03 +0200

    fix production only bug rgb effect

--- portfolio/src/app/components/animated-rgb-border.tsx
+++ portfolio/src/app/components/animated-rgb-border.tsx
@@ -8,7 +8,7 @@ interface RGBBorderProps {
 export function AnimatedRGBBorder({ children, className = "" }: RGBBorderProps) {
   return (
     <div className={`relative ${className} rounded-lg overflow-clip`}>
-      <div className="absolute -inset-0.5 bg-conic/decreasing from-violet-700 via-lime-300 to-violet-700 left-[-50%] top-[-50%] h-[200%] w-[200%] blur animate-spin "></div>
+      <div className="absolute -inset-0.5 bg-gradient-to-r from-red-500 via-blue-500 to-red-500 left-[-50%] top-[-50%] h-[200%] w-[200%] blur animate-spin "></div>
       <div className="relative bg-background rounded-lg m-1 ">{children}</div>
     </div>
   )
